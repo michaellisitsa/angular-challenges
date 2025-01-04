5d2dbf8a11ca81101314fa73bc473f0a91f3ca01
Michael Lisitsa <michael.lisitsa@gmail.com>
2025-01-04 21:49:52 +1100

    feat(1-projection): move img into content projection

--- apps/angular/1-projection/src/app/ui/card/card.component.ts
+++ apps/angular/1-projection/src/app/ui/card/card.component.ts
@@ -1,4 +1,4 @@
-import { NgFor, NgIf } from '@angular/common';
+import { NgFor } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { randStudent, randTeacher } from '../../data-access/fake-http.service';
 import { StudentStore } from '../../data-access/student.store';
@@ -6,20 +6,50 @@ import { TeacherStore } from '../../data-access/teacher.store';
 import { CardType } from '../../model/card.model';
 import { ListItemComponent } from '../list-item/list-item.component';
 
+/*
+Img inserted via content projection, i.e. ng-content to render the children
+
+Since we need to refactor the ListItemComponent as well,
+so it uses content project to render the children,
+we need to have a second content placeholder using the select attribute.
+We will place the ListItemComponent inside this placeholder, and loop it with all the data.
+https://angular.dev/guide/components/content-projection#multiple-content-placeholders
+
+e.g.
+    <app-card
+      [list]="students"
+      customClass="bg-light-green">
+      <app-img>
+        <img src="assets/img/student.webp" width="200px" />
+      </app-img>
+      <app-list-item>
+        <div class="border-grey-300 flex justify-between border px-2 py-1">
+          Name
+          <button (click)="delete(id)">
+            <img class="h-5" src="assets/svg/trash.svg" />
+          </button>
+        </div>
+      </app-list-item>
+    </app-card>
+
+And the delete is bound to the id of the item.
+To use the second part inside of the ListItemComponent, 
+we need to use the ng-templateoutlet directive.
+e.g.
+    <ng-container *ngTemplateOutlet="app-list-item"></ng-container>
+
+
+Good blog about ng-templateoutlet
+https://blog.angular-university.io/angular-ng-template-ng-container-ngtemplateoutlet/
+
+*/
 @Component({
   selector: 'app-card',
   template: `
     <div
       class="flex w-fit flex-col gap-3 rounded-md border-2 border-black p-4"
       [class]="customClass">
-      <img
-        *ngIf="type === CardType.TEACHER"
-        src="assets/img/teacher.png"
-        width="200px" />
-      <img
-        *ngIf="type === CardType.STUDENT"
-        src="assets/img/student.webp"
-        width="200px" />
+      <ng-content select="[card-img]"></ng-content>
 
       <section>
         <app-list-item
@@ -36,7 +66,7 @@ import { ListItemComponent } from '../list-item/list-item.component';
       </button>
     </div>
   `,
-  imports: [NgIf, NgFor, ListItemComponent],
+  imports: [NgFor, ListItemComponent],
 })
 export class CardComponent {
   @Input() list: any[] | null = null;
